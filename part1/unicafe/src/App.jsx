10655738165693fafead54c00d9f089f95f80e27
burmanh <burmanh@outlook.com>
2024-11-02 12:49:43 +0100

    clean up

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 
+//TO DOO
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+//TO DOO
+const goodClick = () => {
+  const newClick = { 
+    left: clicks.left + 1, 
+    right: c
+  }
+  setGood(newClicks)
+}
+
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -7,9 +25,17 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      code here
-    </div>
+    <>
+    <h1>Give Feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good"/>
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral"/>
+      <Button onClick={() => setBad(bad + 1)} text="bad"/>
+ 
+    <h1>Statistics</h1>
+      <p>{good}</p>
+      <p>{neutral}</p>
+      <p>{bad}</p>
+    </>
   )
 }
 
