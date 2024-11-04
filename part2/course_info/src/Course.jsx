be3f75fac8c11b66b4da4dabe636bb3d7777a5f0
burmanh <burmanh@outlook.com>
2024-11-04 18:45:41 +0100

    to do 2.1

---
+++ part2/course_info/src/Course.jsx
@@ -0,0 +1,41 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+//to do MAP så att course parts kan läggas till tas bort
+const Content = ({ parts }) => 
+  <>
+    <Part
+      part={parts[0]} 
+    />
+    <Part
+      part={parts[1]} 
+    />
+    <Part
+      part={parts[2]} 
+    />      
+  </>
+
+
+const Course = ({course}) => {
+  
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={parts} />
+    </div>
+  )
+  /*return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+    </div>
+  )*/
+}
+export default Course
