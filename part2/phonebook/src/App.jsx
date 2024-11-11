5b9c09d5fe15be0dc2ea0fbf4364aaa2d9e2c340
burmanh <burmanh@outlook.com>
2024-11-11 17:53:43 +0100

    2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,65 @@
 import { useState } from 'react'
 
+const ListPersons = ({ person }) => {
+  return (
+    <p>{person.name}</p>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas',
+      id: 1
+    },
+    //{ name: 'henrik burman' }
+  ])
+  //console.log(persons)
   const [newName, setNewName] = useState('')
 
+
+  /** TODO Addera personobjekt till persons-array */  
+  
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: String(persons.length + 1),
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+  //TODO Kolla in det hä'r och varför det behövs
+  const handleNoteChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
-    <div>
+    
+    <>
       <h2>Phonebook</h2>
-      <form>
+      
+      <form onSubmit={addPerson}>
+    
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNoteChange}/>
         </div>
+
         <div>
           <button type="submit">add</button>
+        
         </div>
+        
       </form>
+      
+     
+
       <h2>Numbers</h2>
-      ...
-    </div>
+     {persons.map(person => 
+          <ListPersons key={person.name} person={person} />
+        )}
+
+     <div>debug: {newName}</div>
+    </>
   )
 }
 
