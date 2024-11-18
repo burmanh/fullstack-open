2f898abacf523cfcc4d9821395347e7adb33f8a6
burmanh <burmanh@outlook.com>
2024-11-18 17:27:53 +0100

    2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,19 +2,19 @@ import { useState } from 'react'
 
 const ListPersons = ({ person }) => {
   return (
-    <p>{person.name}</p>
+    <p>{person.name} {person.number}</p>
   )
 }
 
-
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Henrik Burman',
-      id: 1
-    },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   /** Addera personobjekt till persons-array */
   const addPerson = (event) => {
@@ -25,27 +25,29 @@ const App = () => {
       const nameExists = persons.some(person =>
         person.name.toLowerCase() === newName.toLowerCase()
       );
-    /**
-    let nameExists = false; //nameflag
-    for (let i = 0; i < persons.length; i++) {
-      if (persons[i].name.toLowerCase() === newName.toLowerCase()) {
-        nameExists = true;
-        break; // break on match, including lowercase
+      /**
+      let nameExists = false; //nameflag
+      for (let i = 0; i < persons.length; i++) {
+        if (persons[i].name.toLowerCase() === newName.toLowerCase()) {
+          nameExists = true;
+          break; // break on match, including lowercase
+        }
       }
-    }
-    */
+      */
       //"isolate state updates from the rest of addPerson" if name already exists
       if (nameExists) {
         alert(`${newName} is already added to phonebook`);
       } else {
         const personObject = {
-          name: newName,
           id: persons.length + 1,
+          name: newName,
+          number: newNumber,
         };
 
         // Add new name to the phonebook
         setPersons(persons.concat(personObject));
         setNewName('');
+        setNewNumber('');
       }
     }
   };
@@ -54,6 +56,10 @@ const App = () => {
     //console.log('printing from handlePersonChange ' + event.target.value)
     setNewName(event.target.value)
   }
+  const handleNumberChange = (event) => {
+    //console.log('printing from handleNumberChange ' + event.target.value)
+    setNewNumber(event.target.value)
+  }
 
   return (
 
@@ -61,13 +67,16 @@ const App = () => {
       <h2>Phonebook</h2>
 
       <form onSubmit={addPerson}>
+        {/** each <input> element has a single value */}
         <div>
           name: <input value={newName} onChange={handlePersonChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
-
       </form>
 
 
