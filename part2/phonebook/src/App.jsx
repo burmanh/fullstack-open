cd38c803c38f618f79e7c57ca5938f9d72c4b6e8
burmanh <burmanh@outlook.com>
2025-01-07 10:42:48 +0100

    2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,98 +1,30 @@
 import { useState } from 'react'
 import Persons from './Persons'
 import PersonForm from './PersonForm'
-/* OLD
-const ListPersons = ({ person }) => {
-  return (
-    <p>{person.name} {person.number}</p>
-  )
-}
-*/
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
-  /** Addera personobjekt till persons-array OLD *//**
-  const addPerson = (event) => {
-    event.preventDefault();
-
-    if (newName) { // Check if input is NOT empty
-      // Check if the name already exists in the array GPT
-      const nameExists = persons.some(person =>
-        person.name.toLowerCase() === newName.toLowerCase()
-      );
-      //"isolate state updates from the rest of addPerson" if name already exists
-      if (nameExists) {
-        alert(`${newName} is already added to phonebook`);
-      } else {
-        const personObject = {
-          id: persons.length + 1,
-          name: newName,
-          number: newNumber,
-        };
-
-        // Add new name to the phonebook
-        setPersons(persons.concat(personObject));
-        setNewName('');
-        setNewNumber('');
-      }
-    }
-  };
-  *//* OLD
-  const handlePersonChange = (event) => {
-    //console.log('printing from handlePersonChange ' + event.target.value)
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    //console.log('printing from handleNumberChange ' + event.target.value)
-    setNewNumber(event.target.value)
-  }
-  */
+  
   return (
 
     <>
       <h1>Phonebook</h1>
       <p>here goes searchfield</p>
+
       <h2>Add to phonebook</h2>
-      {/*OLD
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handlePersonChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      */}
       <PersonForm
         newName={newName}
         setNewName={setNewName}
         newNumber={newNumber}
         setNewNumber={setNewNumber}
+        persons={persons}
         setPersons={setPersons}
       />
+
       <h2>Numbers</h2>
-      {/*OLD*/}
-      {/* persons.map(person =>
-        <ListPersons key={person.id} person={person} />
-      )*/}
-      {/*OLD*/}
-      {/*persons.map(person =>
-        <SinglePerson key={person.id} person={person} />
-      )*/}
       <Persons persons={persons} />
-
-
-      <div>debug: {/*newName*/}</div>
     </>
   )
 }
--- part2/phonebook/src/PersonForm.jsx
+++ part2/phonebook/src/PersonForm.jsx
@@ -1,5 +1,38 @@
-/** hur ska state skickas setnewname osv?? */
-const PersonForm = ({newName},{newNumber},{setNewName}) => {
+/** hur ska state skickas setnewname osv?? DONE */
+const PersonForm = ({ newName,  setNewName, newNumber, setNewNumber, persons, setPersons}) => {
+    const handlePersonChange = (event) => {
+        //console.log('printing from handlePersonChange ' + event.target.value)
+        setNewName(event.target.value)
+    }
+    const handleNumberChange = (event) => {
+        //console.log('printing from handleNumberChange ' + event.target.value)
+        setNewNumber(event.target.value)
+    }
+    const addPerson = (event) => {
+        event.preventDefault();
+
+        if (newName) { // Check if input is NOT empty
+            // Check if the name already exists in the array GPT
+            const nameExists = persons.some(person =>
+                person.name.toLowerCase() === newName.toLowerCase()
+            );
+            //"isolate state updates from the rest of addPerson" if name already exists
+            if (nameExists) {
+                alert(`${newName} is already added to phonebook`);
+            } else {
+                const personObject = {
+                    id: persons.length + 1,
+                    name: newName,
+                    number: newNumber,
+                };
+
+                // Add new name to the phonebook
+                setPersons(persons.concat(personObject));
+                setNewName('');
+                setNewNumber('');
+            }
+        }
+    };
     return (
         <form onSubmit={addPerson}>
             {/** each <input> element has a single value */}
@@ -16,38 +49,6 @@ const PersonForm = ({newName},{newNumber},{setNewName}) => {
     )
 }
 
-const addPerson = (event) => {
-    event.preventDefault();
 
-    if (newName) { // Check if input is NOT empty
-        // Check if the name already exists in the array GPT
-        const nameExists = persons.some(person =>
-            person.name.toLowerCase() === newName.toLowerCase()
-        );
-        //"isolate state updates from the rest of addPerson" if name already exists
-        if (nameExists) {
-            alert(`${newName} is already added to phonebook`);
-        } else {
-            const personObject = {
-                id: persons.length + 1,
-                name: newName,
-                number: newNumber,
-            };
-
-            // Add new name to the phonebook
-            setPersons(persons.concat(personObject));
-            setNewName('');
-            setNewNumber('');
-        }
-    }
-};
-const handlePersonChange = (event) => {
-    //console.log('printing from handlePersonChange ' + event.target.value)
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    //console.log('printing from handleNumberChange ' + event.target.value)
-    setNewNumber(event.target.value)
-  }
 
 export default PersonForm
